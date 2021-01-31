0b82a289ebbca7fa1150602a92257845b021fd91
Zhang Pluto <lionelzhang99@gmail.com>
2021-01-31 17:52:34 -0500

    Add Test files

---
+++ Kakuro/src/main/java/top/pluto/View/Test.java
@@ -0,0 +1,10 @@
+package top.pluto.View;
+
+/**
+ * @ProjectName FinalProject
+ * @ClassName Test
+ * @Author Information_System_Implementation_Team
+ * @Date 2021-01-31 5:49 p.m.
+ **/
+public class Test {
+}
